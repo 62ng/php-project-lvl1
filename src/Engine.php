5aa3817be6034f88d4e1273a434d340fe50cd41c
Andrey Frolov <internet.buro@gmail.com>
2022-03-11 13:59:47 +0300

    Delete checkRound function

--- src/Engine.php
+++ src/Engine.php
@@ -7,22 +7,6 @@ use function cli\prompt;
 
 const ROUNDS = 3;
 
-function checkRound(string $userName, string $userAnswer, string $correctAnswer): bool
-{
-    if ($userAnswer === $correctAnswer) {
-        line("Correct!");
-        return true;
-    } else {
-        line(
-            "'%s' is wrong answer ;(. Correct answer was '%s'.",
-            $userAnswer,
-            $correctAnswer
-        );
-        line("Let's try again, %s!", $userName);
-        return false;
-    }
-}
-
 function startGame(array $questionsAndAnswers, string $essence): void
 {
     line('Welcome to the Brain Games!');
@@ -36,8 +20,15 @@ function startGame(array $questionsAndAnswers, string $essence): void
         line("Question: %s", $roundQuestion);
         $userAnswer = prompt('Your answer');
 
-        $result = checkRound($userName, $userAnswer, $correctAnswer);
-        if (!$result) {
+        if ($userAnswer === $correctAnswer) {
+            line("Correct!");
+        } else {
+            line(
+                "'%s' is wrong answer ;(. Correct answer was '%s'.",
+                $userAnswer,
+                $correctAnswer
+            );
+            line("Let's try again, %s!", $userName);
             return;
         }
     }
